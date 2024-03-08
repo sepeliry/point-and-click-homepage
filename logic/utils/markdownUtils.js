5cbb2e9493a9de413c5709583ec62692886af11b
ppaabo <paavo.rannikko@icloud.com>
2024-03-08 17:19:53 +0200

    Wikipages with relative links (using []() syntax) to other wikipages, now display the page ingame when clicked

--- logic/utils/markdownUtils.js
+++ logic/utils/markdownUtils.js
@@ -1,84 +1,49 @@
 import { pages } from "../../data/pages";
 import { marked } from "marked";
 
-/**
- * Fetches the raw markdown from github and uses marked to conver it into html string
- * @param {string} url - the rawgithubuserconent url for the markdown wiki page
- * @returns {string} - Markdown content transformed into a html string
- */
-// export const fetchPage = async (url) => {
-//   const response = await fetch(`${url}`);
-//   const data = await response.text();
-//   marked.use({ gfm: true });
-//   const htmlString = marked.parse(data);
-//   return htmlString;
-// };
-
-export const fetchPage = async (url) => {
-  const response = await fetch(`${url}`);
-  const data = await response.text();
-
-  // Create a custom renderer
-  const renderer = new marked.Renderer();
-  const originalLinkRenderer = renderer.link;
-  renderer.link = function(href, title, text) {
-    if (!href.startsWith('http')) {
-      // If the link is a relative link to another wiki page, add a click event listener
-      const pageUrl = findPageUrlByHref(href);
-      if(!pageUrl) return originalLinkRenderer.call(this, href, title, text);
-      else {
-        return `<a href="${href}" onclick="event.preventDefault(); fetchPage('${pageUrl}');">${text}</a>`;
-      } 
-    } else {
-      // If the link is a normal URL, use the original link renderer
-      return originalLinkRenderer.call(this, href, title, text);
-    }
-  };
-  marked.use({ gfm: true, renderer });
-  const htmlString = marked.parse(data);
-  return htmlString;
-};
-
-window.fetchPage = fetchPage;
-// Generates list from wiki page titles. Adds a click listener for each to render the page as html
-export const generateList = () => {
-  const backBtn = document.getElementById("back-btn")
+export const displayPage = async(url) => {
   const pageContainer = document.getElementById('page-container');
   const ulElem = document.getElementById('wiki-list');
+  const backBtn = document.getElementById("back-btn");
+  // Retrieves the page content as html and hides other elements
+  const htmlString = await fetchPage(url);
+  ulElem.style.display = 'none'
+  backBtn.style.display = "none";
+  pageContainer.style.display = 'flex';
+  pageContainer.innerHTML = htmlString;
 
+    // Button for going back to the list
+  const pageBackBtn = document.createElement('button');
+  pageBackBtn.textContent = 'Palaa';
+  pageBackBtn.id = 'page-back-btn';
+  pageBackBtn.classList.add('button');
+  pageBackBtn.addEventListener('click', () => {
+    pageContainer.innerHTML = '';
+    pageContainer.style.display = "none";
+    ulElem.style.display = 'block';
+    pageBackBtn.remove();
+    backBtn.style.display = "";
+  })
+  pageContainer.appendChild(pageBackBtn);
+}
+// Generates list from wiki page titles. Adds a click listener for each to render the page as html
+export const generateList = () => {
+  const ulElem = document.getElementById('wiki-list');
   let htmlList = pages.map((page, index) => {
     return `<li id="page-${index}"><a href="${page.url}">${page.title}</a></li>`;
   }).join('');
   ulElem.innerHTML = htmlList;
 
-  // Adds an event listener to each page title. When clicked opens the wikipage in html
+  // Adds an event listener to each page title. When clicked displays the wikipage as html
   pages.forEach((page, index) => {
     document.getElementById(`page-${index}`).addEventListener('click', async(event) => {
       event.preventDefault();
-      pageContainer.style.display = 'flex';
-      const htmlString  = await fetchPage(page.url);
-      pageContainer.innerHTML = htmlString;
-      ulElem.style.display = 'none'
-      backBtn.style.display = "none";
-
-      // Button for going back to the list
-      const pageBackBtn = document.createElement('button');
-      pageBackBtn.textContent = 'Palaa';
-      pageBackBtn.id = 'page-back-btn';
-      pageBackBtn.classList.add('button');
-      pageBackBtn.addEventListener('click', () => {
-        pageContainer.innerHTML = '';
-        pageContainer.style.display = "none";
-        ulElem.style.display = 'block';
-        pageBackBtn.remove();
-        backBtn.style.display = "";
-      })
-      pageContainer.appendChild(pageBackBtn);
+      await displayPage(page.url);
     })
   })
 }
 
-// Displays the list of wiki page. When a item is clicked, renders the wiki page as html
+// Displays the list of wiki pages. When a item is clicked, renders the wiki page as html
 export const showList = (app, gameContainer) => {
   const container = document.getElementById("markdown-container");
   const showPdf = document.getElementById("show-pdf");
@@ -97,7 +62,6 @@ export const showList = (app, gameContainer) => {
   });
 }
 
-
 const findPageUrlByHref = (href) => {
   const page = pages.find(page => page.url.endsWith(`${href}.md`));
   if(!page){
@@ -105,3 +69,28 @@ const findPageUrlByHref = (href) => {
   }
   return page.url
 }
+
+export const fetchPage = async (url) => {
+  const response = await fetch(`${url}`);
+  const data = await response.text();
+  // Custom renderer for links pointing to other wikipages
+  const renderer = new marked.Renderer();
+  const originalLinkRenderer = renderer.link;
+  renderer.link = function(href, title, text) {
+    if (!href.startsWith('http')) {
+      // If href points to a wikipage and the pages url is found, clicking the link opens page ingame
+      const pageUrl = findPageUrlByHref(href);
+      if(!pageUrl) return originalLinkRenderer.call(this, href, title, text);
+      else {
+        return `<a href="${href}" onclick="event.preventDefault(); displayPage('${pageUrl}');">${text}</a>`;
+      } 
+    } else {
+      return originalLinkRenderer.call(this, href, title, text);
+    }
+  };
+  marked.use({ gfm: true, renderer });
+  const htmlString = marked.parse(data);
+  return htmlString;
+};
+
+window.displayPage = displayPage;

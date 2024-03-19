149c4db8309463108e64a08b6a553ccf6d18930d
artisokka <janika.l.ranta@gmail.com>
2024-03-19 13:18:10 +0200

    Bookshelf/numpad view mapped to canvas + back arrow button

--- logic/UI.js
+++ logic/UI.js
@@ -2,6 +2,7 @@ import * as PIXI from "pixi.js";
 import backgroundImg from "../resources/images/background.png";
 import bookshelfBackgroundImg from "../resources/images/bookshelf.jpg";
 import numpadBackgroundImg from "../resources/images/num_pad.png";
+import back_arrowImg from "../resources/images/back_arrow.png";
 
 class UI {
   constructor(app) {
@@ -23,12 +24,67 @@ class UI {
     bookshelfBackground.height = app.screen.height;
     app.bookshelfContainer.addChild(bookshelfBackground);
 
+    // Back button for bookshelf
+    const backArrowTexture = PIXI.Texture.from(back_arrowImg);
+    const button = new PIXI.Sprite(backArrowTexture);
+    button.x = 0.03 * app.screen.width;
+    button.y = 0.03 * app.screen.height;
+    button.interactive = true;
+    button.cursor = "pointer";
+    button.buttonMode = true;
+    button.addEventListener("click", this.toggleBookshelf(app));
+    app.bookshelfContainer.addChild(button);
+
     // Create sprite for num pad view
     const numpadTexture = PIXI.Texture.from(numpadBackgroundImg);
     const numpadBackground = new PIXI.Sprite(numpadTexture);
     numpadBackground.width = app.screen.width;
     numpadBackground.height = app.screen.height;
     app.numpadContainer.addChild(numpadBackground);
+
+    // Back button for numpad
+    const buttonNumpad = new PIXI.Sprite(backArrowTexture);
+    buttonNumpad.x = 0.03 * app.screen.width;
+    buttonNumpad.y = 0.03 * app.screen.height;
+    buttonNumpad.interactive = true;
+    buttonNumpad.cursor = "pointer";
+    buttonNumpad.buttonMode = true;
+    buttonNumpad.addEventListener("click", this.toggleNumpad(app));
+    app.numpadContainer.addChild(buttonNumpad);
+
+    // Create clickable area on bookshelf
+    const bookshelfMapping = new PIXI.Graphics();
+    const width = (12.5 / 100) * app.screen.width;
+    const height = (30 / 100) * app.screen.height;
+    bookshelfMapping.beginFill(0x00FF00);
+    bookshelfMapping.drawRect(0, 0, width, height);
+    bookshelfMapping.endFill();
+    bookshelfMapping.alpha = 0;
+    bookshelfMapping.visible = true;
+    bookshelfMapping.x = app.screen.width / 3.72;
+    bookshelfMapping.y = app.screen.height / 2.5;
+    bookshelfMapping.interactive = true;
+    bookshelfMapping.buttonMode = true;
+    bookshelfMapping.cursor = "pointer";
+    bookshelfMapping.addEventListener("click", this.toggleBookshelf(app));
+    app.gameContainer.addChild(bookshelfMapping);
+
+    // Create clickable area on door numpad
+    const numpadMapping = new PIXI.Graphics();
+    const w = (2 / 100) * app.screen.width;
+    const h = (6 / 100) * app.screen.height;
+    numpadMapping.beginFill(0x00FF00);
+    numpadMapping.drawRect(0, 0, w, h);
+    numpadMapping.endFill();
+    numpadMapping.alpha = 0;
+    numpadMapping.visible = true;
+    numpadMapping.x = app.screen.width / 1.94;
+    numpadMapping.y = app.screen.height / 1.87;
+    numpadMapping.interactive = true;
+    numpadMapping.buttonMode = true;
+    numpadMapping.cursor = "pointer";
+    numpadMapping.addEventListener("click", this.toggleNumpad(app));
+    app.gameContainer.addChild(numpadMapping);
   }
 
   toggleBookshelf(app) {

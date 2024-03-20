4fcb15b68af73b112c9be67a77e45dda682a7fcf
ppaabo <paavo.rannikko@icloud.com>
2024-03-20 14:23:12 +0200

    Datastructure for PDF Files and a new view for displaying pdf files ingame

---
+++ data/pdfFiles.js
@@ -0,0 +1,18 @@
+export const pdfFiles = [
+  {
+    title: "Pelimekaniikan suunnittelua",
+    path: "./docs/input/pelienSuunnittelu.pdf",
+  },
+  {
+    title: "Sepeli Homepage web and mobile layout drafts",
+    path: "./docs/input/Sepeli homepage web and mobile layout drafts.pdf",
+  },
+  {
+    title: "Sepeli point and click homepage idea paper",
+    path: "./docs/input/Sepeli point and click homepage idea paper.pdf",
+  },
+  {
+    title: "Sepeli point and click homepage presentation",
+    path: "./docs/input/Sepeli point and click homepage presentation.pdf",
+  },
+];
--- logic/utils/pdfUtils.js
+++ logic/utils/pdfUtils.js
@@ -1,13 +1,15 @@
 // Pikaiset kokeilut PDF esittämiseen käyttäen HTMl <Object>
 // PDFJs avulla luultavasti saa viimeisellymmän näkymän ja sulavemmin sisälletyä sisällöt peliin
-
+import { pdfFiles } from "../../data/pdfFiles";
+import * as PIXI from "pixi.js";
+const showPdfBtn = document.getElementById("show-pdf");
+const closePdfBtn = document.getElementById("close-pdf");
+const pdfViewer = document.getElementById("pdf-viewer-container");
+const pdfObject = document.getElementById("pdf-object");
+const gameContainerHTML = document.getElementById("game-container");
+let pdfContainer = new PIXI.Container();
+// Displays a pdf file
 export function showPdf(app, gameContainer, pdfPath) {
-  const pdfViewer = document.getElementById("pdf-viewer-container");
-  const showPdf = document.getElementById("show-pdf");
-  const pdfObject = document.getElementById("pdf-object");
-  const gameContainerHTML = document.getElementById("game-container");
-  // Hides showpdf button, game stage and opens the pdf viewer
-  showPdf.style.display = "none";
   pdfViewer.style.display = "flex";
   gameContainer.eventMode = "none";
   app.stage.visible = "false";
@@ -16,26 +18,36 @@ export function showPdf(app, gameContainer, pdfPath) {
 }
 // Closes the pdf viewer and makes the game visible again
 export function closePdf(app, gameContainer) {
-  const pdfViewer = document.getElementById("pdf-viewer-container");
-  const showPdf = document.getElementById("show-pdf");
-  const gameContainerHTML = document.getElementById("game-container");
-  showPdf.style.display = "";
   pdfViewer.style.display = "none";
   gameContainerHTML.style.display = "";
   app.stage.visible = "true";
   gameContainer.eventMode = "static";
 }
-// Kokeiluja varten setuppaa buttonit pdf avaamiseen ja sulkemiseen
+
+// Creates text elements to open a single pdf file
 export const setupPdf = (app, gameContainer) => {
-  const pelienSuunittelu = "./docs/input/pelienSuunnittelu.pdf";
+  pdfFiles.forEach((pdfFile, index) => {
+    let text = new PIXI.Text(pdfFile.title, { fill: "#ffffff" });
+    text.eventMode = "static";
+    text.cursor = "pointer";
+    text.y = index * 30;
 
-  document
-    .getElementById("show-pdf")
-    .addEventListener("click", () =>
-      showPdf(app, gameContainer, pelienSuunittelu)
-    );
+    text.on("pointerdown", () => {
+      showPdf(app, gameContainer, pdfFile.path);
+    });
+    pdfContainer.addChild(text);
+  });
 
-  document.getElementById("close-pdf").addEventListener("click", () => {
+  app.stage.addChild(pdfContainer);
+  pdfContainer.visible = false;
+
+  closePdfBtn.addEventListener("click", () => {
     closePdf(app, gameContainer);
+  });
+  // Toggles the view between PDF Container and gameContainer
+  showPdfBtn.addEventListener("click", () => {
+    closePdf(app, gameContainer);
+    pdfContainer.visible = !pdfContainer.visible;
+    gameContainer.visible = !gameContainer.visible;
   });
 };

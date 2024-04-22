e11a407a2284b2da6d7c97a79bd960578d100eed
Kristian Skogberg <kristian.skogberg@tuni.fi>
2024-04-22 23:59:58 +0300

    more unit tests for player and openUrlInNewTab

--- logic/interactions/removeSprite.js
+++ logic/interactions/removeSprite.js
@@ -1,10 +1,9 @@
 function removeSprite(app, sprite) {
   Object.entries(app.scenes).forEach(([sceneName, scene]) => {
-    //console.log("Processing scene:", sceneName);
-
     if (scene.children) {
       if (scene.children.includes(sprite)) {
-        scene.removeChild(sprite);
+        sprite.visible = false;
+        //scene.removeChild(sprite);
       }
     }
   });

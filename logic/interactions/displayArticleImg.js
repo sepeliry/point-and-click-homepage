ef168bdb1c3dd4f17c64edce8f44afa63dd75902
kristian_skogberg <kristian.skogberg@tuni.fi>
2024-04-30 22:41:09 +0300

    test and fix wiki text processing and change styling of close buttons

--- logic/interactions/displayArticleImg.js
+++ logic/interactions/displayArticleImg.js
@@ -1,9 +1,9 @@
 // Function to display article images in HTML containers
 function displayArticleImg(img) {
-    const articleImgContainer = document.getElementById("article-img-container");
-    articleImgContainer.style.display = "block";
-    var articleImg = document.getElementById("article-img");
-    articleImg.src = img;
+  const articleImgContainer = document.getElementById("article-img-container");
+  articleImgContainer.style.display = "block";
+  var articleImg = document.getElementById("article-img");
+  articleImg.src = img;
 }
 
 export default displayArticleImg;
